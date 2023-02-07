2c317826013a9d2007a0ab44c58bdf4169d87a88
bahihi99 <45497129+bahihi99@users.noreply.github.com>
2023-02-07 21:07:40 +0100

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,21 @@
+@echo off
+
+if %1=="" (
+	set /p lang="choose a language: "
+) else (
+	set lang=%1
+)
+
+for %%a in (%list%) do (
+	if /i "%%a" == "%lang%" (
+		echo.
+		reg add HKCU\Software\7-Zip /t REG_SZ /v Lang /d %lang% /f
+		echo 7-Zip language set to %lang%
+		goto :EOF		
+	)
+)
+
+echo please enter a valid language code.
+echo language codes available: [%list%]
+
+pause
