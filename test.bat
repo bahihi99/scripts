0d372960762136921fd0b7deb25b29fa5918bda2
bahihi99 <45497129+bahihi99@users.noreply.github.com>
2023-02-07 21:18:24 +0100

    changed tabs to spaces and code fix

--- test.bat
+++ test.bat
@@ -1,18 +1,20 @@
 @echo off
 
-if %1=="" (
-	set /p lang="choose a language: "
+set list=en fr it pt de nl
+
+if "%1"=="" (
+    set /p lang="choose a language: "
 ) else (
-	set lang=%1
+    set lang=%1
 )
 
 for %%a in (%list%) do (
-	if /i "%%a" == "%lang%" (
-		echo.
-		reg add HKCU\Software\7-Zip /t REG_SZ /v Lang /d %lang% /f
-		echo 7-Zip language set to %lang%
-		goto :EOF		
-	)
+    if /i "%%a" == "%lang%" (
+        echo.
+        reg add HKCU\Software\7-Zip /t REG_SZ /v Lang /d %lang% /f >null
+        echo 7-Zip language set to %lang%
+        goto :EOF       
+    )
 )
 
 echo please enter a valid language code.
